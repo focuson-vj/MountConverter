b20d8f8901044c2327cf10da0646a1abecd5bb65
k2font <shoichiro.kouno@gmail.com>
2020-05-24 17:57:44 +0900

    helmetを仕込みX-Powered-Byヘッダを除去した

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
